11033fd15c4dd11ffdaaaac74993a84ae5d7870f
KevinZjYang <keivn.zj.yang@outlook.com>
2020-04-21 19:05:16 +0800

    settings add input and output data method

--- AnimalCrossing/ViewModels/SettingsViewModel.cs
+++ AnimalCrossing/ViewModels/SettingsViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -7,8 +8,11 @@ using AnimalCrossing.Services;
 
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
-
+using SQLite;
 using Windows.ApplicationModel;
+using Windows.Storage;
+using Windows.Storage.AccessCache;
+using Windows.Storage.Pickers;
 using Windows.UI.Xaml;
 
 namespace AnimalCrossing.ViewModels
@@ -108,6 +112,84 @@ namespace AnimalCrossing.ViewModels
             SettingsHelper.SaveLocalSetting(SettingsKey.DefaultHemisphereKey, "North");
         }
 
+        private ICommand _outputData;
+        public ICommand OutputDataCommand
+        {
+            get
+            {
+                if(_outputData == null)
+                {
+                    _outputData = new RelayCommand(async () =>
+                    {
+                        var file = await ApplicationData.Current.LocalFolder.GetFileAsync("User.db");
+                        FolderPicker picker = new FolderPicker();
+                        picker.SuggestedStartLocation = PickerLocationId.Desktop;
+                        picker.FileTypeFilter.Add(".db");
+                        var folder = await picker.PickSingleFolderAsync();
+                        if (folder != null)
+                        {
+                            await file.CopyAsync(folder,"User.db",NameCollisionOption.ReplaceExisting);
+                            
+                            var notify = new NotifyPopup($"导出用户数据到: {folder.Path}",TimeSpan.FromSeconds(5));
+                            notify.Show();
+                        }
+                        else
+                        {
+                            var notify = new NotifyPopup("导出失败，请重试");
+                            notify.Show(); 
+                        }
+                    });
+                }
+                return _outputData;
+            }
+            
+        }
+
+        private ICommand _inputData;
+        public ICommand InputDataCommand
+        {
+            get
+            {
+                if(_inputData == null)
+                {
+                    _inputData = new RelayCommand(async () =>
+                    {
+                        NotifyPopup np;
+                        try
+                        {
+                          
+                            var localFolder = ApplicationData.Current.LocalFolder;
+                            FileOpenPicker openPicker = new FileOpenPicker();
+                            openPicker.FileTypeFilter.Add(".db");
+                            StorageFile file = await openPicker.PickSingleFileAsync();
+                            
+                            var result = await file.CopyAsync(localFolder, "User1.db", NameCollisionOption.ReplaceExisting);
+                            using (var liteConnection = new SQLiteConnection(result.Path))
+                            {
+                                var info1 = liteConnection.GetTableInfo("Insect");
+                                var info2 = liteConnection.GetTableInfo("Fish");
+                                if (info1.Count == 0 && info2.Count == 0)
+                                {
+                                    np = new NotifyPopup("导入数据无效,请重试", TimeSpan.FromSeconds(5));
+                                    np.Show();
+                                    return;
+                                }
+                            }
+                            await result.RenameAsync("User.db", NameCollisionOption.ReplaceExisting);
+                            np = new NotifyPopup("导入成功");
+                            np.Show();
+                        }
+                        catch (Exception e)
+                        {
+
+                            np = new NotifyPopup($"导入失败,{e.Message}");
+                            np.Show();
+                        }
+                    });
+                }
+                return _inputData;
+            }
+        }
         public SettingsViewModel()
         {
         }
